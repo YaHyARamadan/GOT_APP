e943c05708cbe19e0a92d84085161463af838a85
YaHyARamadan <yahyaramadan.m>
2024-10-29 11:24:12 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/got_api_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.got_api_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/routes/router.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:got_api_app/core/routes/routes.dart';
+
+import '../../features/main_screen/view/main_screen.dart';
+
+
+Route<dynamic> onGenerate(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AppRoutes.mainScreen:
+      return MaterialPageRoute(
+          builder: (context) =>  const MainScreen(), settings: routeSettings);
+
+    default:
+      return MaterialPageRoute(
+          builder: (context) =>  const MainScreen(), settings: routeSettings);
+  }
+}
